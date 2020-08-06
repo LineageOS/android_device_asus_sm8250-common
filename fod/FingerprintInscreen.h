0b9c364dbef187b5d358287afe036462efd865c1
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-08-06 13:41:45 +0200

    sm8250-common: Initial FOD hooks
    Change-Id: I89a65e20e07493ba1e87f116f6178964d7af59a0

--- fod/FingerprintInscreen.h
+++ fod/FingerprintInscreen.h
@@ -17,6 +17,7 @@
 #define VENDOR_LINEAGE_BIOMETRICS_FINGERPRINT_INSCREEN_V1_0_FINGERPRINTINSCREEN_H
 
 #include <vendor/lineage/biometrics/fingerprint/inscreen/1.0/IFingerprintInscreen.h>
+#include <vendor/goodix/hardware/biometrics/fingerprint/2.1/IGoodixFingerprintDaemon.h>
 
 namespace vendor {
 namespace lineage {
@@ -29,10 +30,11 @@ namespace implementation {
 using ::android::sp;
 using ::android::hardware::Return;
 using ::android::hardware::Void;
+using ::vendor::goodix::hardware::biometrics::fingerprint::V2_1::IGoodixFingerprintDaemon;
 
 class FingerprintInscreen : public IFingerprintInscreen {
   public:
-    FingerprintInscreen() = default;
+    FingerprintInscreen();
 
     Return<void> onStartEnroll() override;
     Return<void> onFinishEnroll() override;
@@ -49,6 +51,9 @@ class FingerprintInscreen : public IFingerprintInscreen {
     Return<int32_t> getPositionX() override;
     Return<int32_t> getPositionY() override;
     Return<int32_t> getSize() override;
+
+  private:
+    sp<IGoodixFingerprintDaemon> mGoodixFingerprintDaemon;
 };
 
 }  // namespace implementation
