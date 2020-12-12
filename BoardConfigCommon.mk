4e08fcc45d21435ce415175cf8ea0887d78ce736
Alessandro Astone <ales.astone@gmail.com>
2020-12-13 00:17:36 +0100

    sm8250-common: Update sepolicy for eleven
    Change-Id: Ifc02832f5f4a2391c67d0ef8773c6b424d85e4dc

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -200,6 +200,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Verified Boot
