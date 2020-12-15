66f35f42b0b014ea09cde111894eed54864d0064
Alessandro Astone <ales.astone@gmail.com>
2020-12-15 22:38:27 +0100

    sm8250-common: Add libgui shim
    Change-Id: I1b78ee29a58eb18ffc59dccf0c0214a98372262f

--- common.mk
+++ common.mk
@@ -198,6 +198,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Shims
+PRODUCT_PACKAGES += \
+    libgui_shim
+
 # System Helper
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.systemhelper@1.0
