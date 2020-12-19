5d64bddb714e439be0640e09c17dd05fde90cee2
Michael Bestas <mkbestas@lineageos.org>
2020-12-19 02:53:35 +0200

    sm8250-common: Switch to standalone extract utils
    Align with templates while we are at it.
    
    Change-Id: Ife50623a3d1b8733f5cc76995c982d8950d39ad4

--- regen-vendor.sh
+++ regen-vendor.sh
@@ -20,9 +20,9 @@ set -e
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
