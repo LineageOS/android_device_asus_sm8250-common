c1f14a4340eed547952733a0e6463f4308f22066
Alessandro Astone <ales.astone@gmail.com>
2021-04-24 21:28:35 +0200

    sm8250-common: Update blobs and configs from Android 11 release
    * From UL-ASUS_I003_1-ASUS-18.0410.2109.172-1.1.5-user
    
    Change-Id: I03634c77e0846dc05129d8e89521bb9dcd6512ff

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -144,6 +144,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/vendor_framework_compatibility_matrix.xml
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
